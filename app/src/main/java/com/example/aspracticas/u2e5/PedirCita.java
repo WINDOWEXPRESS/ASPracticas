0e35f9dc87425cfb33191de765e7a686823258c8
BIG BOSS <zhouyichen1998@gmail.com>
2023-10-06 15:17:27 +0200

    "Empezar pedir cita en casa"

---
+++ app/src/main/java/com/example/aspracticas/u2e5/PedirCita.java
@@ -0,0 +1,16 @@
+package com.example.aspracticas.u2e5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aspracticas.R;
+
+public class PedirCita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.u2a5_pedir_cita);
+    }
+}
