cd88cf2530fe927af8d29c2d295743403596394a
XUEXIAO <zhouyichen1998@gmail.com>
2023-10-24 18:00:22 +0200

    terminar primer parte de heladeria y pedir nombre || insti

---
+++ app/src/main/java/com/example/aspracticas/ut03/u3e4/PedirNombre.java
@@ -0,0 +1,16 @@
+package com.example.aspracticas.ut03.u3e4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aspracticas.R;
+
+public class PedirNombre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.u3e4_pedir_nombre);
+    }
+}
