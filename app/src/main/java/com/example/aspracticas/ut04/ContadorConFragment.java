5b9b4d54ed26b6a89bf000fbb3d64e75dcb5530a
at4dam2 <at2@gg.com>
2023-11-23 21:43:31 +0100

    Terminar u4_ejemplo_fragments.xml en insti

---
+++ app/src/main/java/com/example/aspracticas/ut04/ContadorConFragment.java
@@ -0,0 +1,16 @@
+package com.example.aspracticas.ut04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aspracticas.R;
+
+public class ContadorConFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.u4a1_contador_con_fragment);
+    }
+}
