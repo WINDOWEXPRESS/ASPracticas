67ba4c72832c8350f70f353335aef5305af3ae86
BIG BOSS <zhouyichen1998@gmail.com>
2023-12-02 21:23:16 +0100

    "Prueba de branch PiedraPepelTijera_v1"

--- app/src/main/java/com/example/aspracticas/ut02/u2e5/ValidacionDni.java
+++ app/src/main/java/com/example/aspracticas/ut02/u2e5/ValidacionDni.java
@@ -15,3 +15,30 @@ public class ValidacionDni {
                 && dni.charAt(8) == DIGITO_CONTROL.charAt(Integer.parseInt(dni.substring(0, 8)) % 23); // (3)
     }
 }
+
+/*
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EjemploRegex {
+    public static void main(String[] args) {
+        // Cadena de entrada
+        String texto = "La temperatura actual es 25°C, pero se espera que suba a 30°C más tarde.";
+
+        // Definir la expresión regular
+        String patron = "\\d+°C";
+
+        // Compilar la expresión regular en un objeto Pattern
+        Pattern pattern = Pattern.compile(patron);
+
+        // Crear un objeto Matcher para la cadena de entrada
+        Matcher matcher = pattern.matcher(texto);
+
+        // Buscar todas las ocurrencias
+        while (matcher.find()) {
+            // Obtener el texto coincidente
+            String coincidencia = matcher.group();
+            System.out.println("Encontrado: " + coincidencia);
+        }
+    }
+}*/
